9e27ed3858f3ecaf04a17ff4ec2c82800a9be574
Jin <jinyuli91@gmail.com>
2023-06-06 22:22:23 -0500

    update counting for race condition

--- mass_close_prs.py
+++ mass_close_prs.py
@@ -1,9 +1,11 @@
 import requests
 import json
 import concurrent.futures
+import threading
 
 
 pr_count = 0
+pr_count_lock = threading.Lock()
 
 def close_pull_request(pr):
     headers = {
@@ -13,8 +15,7 @@ def close_pull_request(pr):
     
     # Get pull request repository information 
     repo = requests.get(pr["repository_url"], headers=headers).json()
-    repo_full_name = repo["full_name"]
-    pr_number = pr["number"]
+    repo_full_name, pr_number, pr_name = repo["full_name"], pr["number"], pr["name"]
 
     # Change pull request state to closed
     close_pr_url = f"https://api.github.com/repos/{repo_full_name}/pulls/{pr_number}"
@@ -22,9 +23,12 @@ def close_pull_request(pr):
     response = requests.patch(close_pr_url,body, headers=headers)
 
     if response.status_code == 200:
-        print(f"Pull request #{pr_number} closed successfully.")
+        print(f"Pull request: {pr_name} closed successfully.")
+        # Stop race condition when adding successful count
+        with pr_count_lock:
+            pr_count += 1
     else:
-        print(f"Failed to close pull request #{pr_number}.")
+        print(f"Failed to close pull request {pr_name}.")
 
 def close_all_pull_requests(username, access_token):
     headers = {
@@ -44,6 +48,8 @@ def close_all_pull_requests(username, access_token):
     # Github returns a max of 30 pull request per call
     if len(prs) > 0: 
         close_all_pull_requests(username=username, access_token=access_token)
+    
+    print(f"You removed {pr_count} PRs")
 
 # Replace with your GitHub username and access token
 
